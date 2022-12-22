21e1e5dd6db42b7ea13688619fff863dcf84d980
Ksenia Popova <98548104+KseniaPopova02@users.noreply.github.com>
2022-12-22 14:32:46 +0100

    Done!

--- app/src/components/LoanPopup/LoanPopup.js
+++ app/src/components/LoanPopup/LoanPopup.js
@@ -1,8 +1,167 @@
 import styled from "styled-components";
+import React from "react";
+import { useState, useRef } from "react";
+
+//LoansPopup style
+
+//LoansPopupWrapper
+const LoanPopupWrapper = styled.div`
+  margin: 64px;
+  width: 50vw;
+
+  border-radius: 5px;
+  margin: auto;
+  background-color: #fff;
+  z-index: 1000;
+  color: #303247;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+  padding: 38px;
+  @media (max-width: 760px) {
+    padding: 15px;
+    width: 85vw;
+  }
+`;
+//LoanPopupCross
+const LoanPopupCross = styled.div`
+  position: absolute;
+  right: 20px;
+  top: 35px;
+  width: 32px;
+  height: 32px;
+  opacity: 0.3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+//Button
+const Button = styled.button`
+  background-color: transparent;
+  width: 100%;
+  height: 100%;
+  border: none;
+
+  :before,
+  :after {
+    position: absolute;
+    top: 5px;
+    left: 15px;
+    content: " ";
+    height: 22px;
+    width: 2px;
+    background-color: #000;
+  }
+  &:hover::before,
+  &:hover::after {
+    background-color: #1c1e2a;
+  }
+  :before {
+    transform: rotate(45deg);
+  }
+  :after {
+    transform: rotate(-45deg);
+  }
+`;
+//LoanPopupTitle
+const LoanPopupTitle = styled.div`
+  font-weight: 600;
+  font-size: 35px;
+  line-height: 42px;
+  @media (max-width: 800px) {
+    padding-bottom: 0;
+    font-size: 27px;
+    p {
+      padding: 0;
+    }
+  }
+`;
+//LoanPopupSubtitle
+const LoanPopupSubtitle = styled.div`
+  padding-bottom: 10px;
+  @media (max-width: 800px) {
+    padding-bottom: 0;
+  }
+  @media (max-width: 497px) {
+    font-size: 27px;
+  }
+`;
+//LoanPopupInfo
+const LoanPopupInfo = styled.div`
+  p {
+    padding-bottom: 8px;
+  }
+`;
+//LoanInvestments
+const LoanInvestments = styled.div`
+  margin-top: 64px;
+  @media (max-width: 800px) {
+    margin-top: 0;
+  }
+`;
+//LoanInvestmentsSubtitle
+const LoanInvestmentsSubtitle = styled.div`
+  font-size: 30px;
+  line-height: 36px;
+  padding-bottom: 30px;
+  font-weight: 600;
+  @media (max-width: 800px) {
+    font-size: 25px;
+  }
+`;
+//LoanInvestmentForm
+const LoanInvestmentForm = styled.form`
+  display: flex;
+  justify-content: flex-end;
+  @media (max-width: 1040px) {
+    flex-direction: column;
+  }
+`;
+
+//LoanInvestmentInput
+const LoanInvestmentInput = styled.input`
+  width: 100%;
+  padding: 16px 32px;
+  border: 1px solid rgba(48, 50, 71, 0.1);
+  border-radius: 5px;
+  text-align: left;
+  padding-left: 32px;
+  font-size: 22px;
+  text-align: right;
+  @media (max-width: 1040px) {
+    margin-bottom: 10px;
+    padding: 10px 20px;
+  }
+`;
+
+const LoansBtn = styled.button`
+  padding: 16px 32px;
+  border-radius: 5px;
+  text-transform: uppercase;
+  background-color: #f6e308;
+  border: none;
+  font-size: 20px;
+  font-weight: 500;
+  margin-left: 35px;
+  &:hover {
+    background-color: #e5d736;
+  }
+  @media (max-width: 1040px) {
+    margin: 0;
+  }
+
+  @media (max-width: 590px) {
+    padding: 10px 20px;
+    font-size: 16px;
+  }
+`;
 
 function LoanPopup(props) {
-  //LoansPopup style
-  let LoanPopup = styled.div`
+  // ref
+  const value = useRef("");
+
+  //styling
+  const LoanPopup = styled.div`
     display: ${props.show ? "block" : "none"};
     width: 100%;
     height: 100%;
@@ -21,124 +180,6 @@ function LoanPopup(props) {
       width: 100%;
       height: 100%;
       background: rgba(26, 26, 26, 0.4);
-    }
-  `;
-  //LoansPopupWrapper
-  let LoanPopupWrapper = styled.div`
-    margin: 64px;
-    width: 50vw;
-
-    border-radius: 5px;
-    margin: auto;
-    background-color: #fff;
-    z-index: 1000;
-    color: #303247;
-    position: relative;
-    top: 50%;
-    transform: translateY(-50%);
-    padding: 38px;
-    @media (max-width: 760px) {
-      padding: 15px;
-      width: 85vw;
-    }
-  `;
-  //LoanPopupCross
-  let LoanPopupCross = styled.div`
-    position: absolute;
-    right: 20px;
-    top: 0;
-    width: 32px;
-    height: 32px;
-    opacity: 0.3;
-  `;
-  //Button
-  let Button = styled.button`
-    background-color: transparent;
-    border: none;
-    &:hover {
-      opacity: 1;
-    }
-    :before,
-    :after {
-      position: absolute;
-      left: 15px;
-      content: " ";
-      height: 22px;
-      width: 2px;
-      background-color: #303247;
-    }
-    :before {
-      transform: rotate(45deg);
-    }
-    :after {
-      transform: rotate(-45deg);
-    }
-  `;
-  //LoanPopupTitle
-  let LoanPopupTitle = styled.div`
-    font-weight: 600;
-    font-size: 35px;
-    line-height: 42px;
-    @media (max-width: 800px) {
-      padding-bottom: 0;
-      font-size: 27px;
-      p {
-        padding: 0;
-      }
-    }
-  `;
-  //LoanPopupSubtitle
-  let LoanPopupSubtitle = styled.div`
-    padding-bottom: 10px;
-    @media (max-width: 800px) {
-      padding-bottom: 0;
-    }
-    @media (max-width: 497px) {
-      font-size: 27px;
-    }
-  `;
-  //LoanPopupInfo
-  let LoanPopupInfo = styled.div`
-    p {
-      padding-bottom: 8px;
-    }
-  `;
-  //LoanInvestments
-  let LoanInvestments = styled.div`
-    margin-top: 64px;
-    @media (max-width: 800px) {
-      margin-top: 0;
-    }
-  `;
-  //LoanInvestmentsSubtitle
-  let LoanInvestmentsSubtitle = styled.div`
-    font-size: 30px;
-    line-height: 36px;
-    padding-bottom: 30px;
-    font-weight: 600;
-    @media (max-width: 800px) {
-      font-size: 25px;
-    }
-  `;
-  //LoanInvestmentForm
-  let LoanInvestmentForm = styled.form`
-    display: flex;
-    justify-content: space-between;
-    @media (max-width: 1040px) {
-      flex-direction: column;
-    }
-  `;
-
-  //LoanInvestmentInput
-  let LoanInvestmentInput = styled.input`
-    padding: 16px 32px;
-    border: 1px solid rgba(48, 50, 71, 0.1);
-    border-radius: 5px;
-    text-align: left;
-    padding-left: 32px;
-    font-size: 22px;
-    @media (max-width: 1040px) {
-      margin-bottom: 10px;
     }
   `;
 
@@ -165,15 +206,27 @@ function LoanPopup(props) {
             <p>{props.loan.title}</p>
           </LoanPopupSubtitle>
           <LoanPopupInfo>
-            <p>Amount available: {props.loan.amount}</p>
+            <p>Amount available: {props.loan.available}$</p>
             <p>Loan ends in: {timeRemaining}</p>
           </LoanPopupInfo>
         </div>
         <LoanInvestments>
           <LoanInvestmentsSubtitle>Investment amount</LoanInvestmentsSubtitle>
-          <LoanInvestmentForm>
-            <LoanInvestmentInput type="text" placeholder="1,000" />
-            <button className="loans__btn">Invest</button>
+          <LoanInvestmentForm
+            onSubmit={(e) => {
+              e.preventDefault();
+              props.changeInvestedValue(props.loan.id, value.current);
+              props.setShowPopup(false);
+            }}
+          >
+            <LoanInvestmentInput
+              onInput={(event) => {
+                value.current = event.target.value;
+              }}
+              type="number"
+              placeholder="1,000"
+            />
+            <LoansBtn type="submit">Invest</LoansBtn>
           </LoanInvestmentForm>
         </LoanInvestments>
       </LoanPopupWrapper>
